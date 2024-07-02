c56740e9cc7061f08870377e8c5b67cb02da7a10
gmhridu <hasanhridoymahabub9@gmail.com>
2024-07-02 23:04:42 +0600

    Create Technologies section with icons

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,37 +1,55 @@
-import React from 'react';
-import { DiRedis } from 'react-icons/di';
-import { FaNodeJs } from 'react-icons/fa';
-import { IoLogoFirebase } from 'react-icons/io5';
-import {RiReactjsLine} from 'react-icons/ri'
-import { SiMongodb, SiTailwindcss } from 'react-icons/si';
-import { TbBrandNextjs } from 'react-icons/tb';
+import React from "react";
+import { DiRedis } from "react-icons/di";
+import { FaNodeJs } from "react-icons/fa";
+import { IoLogoFirebase } from "react-icons/io5";
+import { RiReactjsLine } from "react-icons/ri";
+import { SiMongodb, SiTailwindcss } from "react-icons/si";
+import { TbBrandNextjs } from "react-icons/tb";
 
 const Technologies = () => {
-    return (
-        <div className='border-b border-neutral-800 pb-24 cursor-pointer'>
-            <h2 className='my-20 text-center text-4xl'>Technologies</h2>
-            <div className='flex flex-wrap items-center justify-center gap-4'>
-            <div title='Tailwind CSS' className='rounded-2xl border-4 border-neutral-800 p-4'>
-                  <SiTailwindcss className='text-7xl text-cyan-400'/>  
-                </div>
-                <div title='React JS' className='rounded-2xl border-4 border-neutral-800 p-4'>
-                  <RiReactjsLine className='text-7xl text-cyan-400'/>  
-                </div>
-                <div title='NextJs' className='rounded-2xl border-4 border-neutral-800 p-4'>
-                  <TbBrandNextjs className='text-7xl text-cyan-400'/>  
-                </div>
-                <div title='Firebase' className='rounded-2xl border-4 border-neutral-800 p-4'>
-                  <IoLogoFirebase className='text-7xl text-cyan-400'/>  
-                </div>
-                <div title='MongoDB' className='rounded-2xl border-4 border-neutral-800 p-4'>
-                  <SiMongodb className='text-7xl text-cyan-400'/>  
-                </div>
-                <div title='NodeJs' className='rounded-2xl border-4 border-neutral-800 p-4'>
-                  <FaNodeJs className='text-7xl text-cyan-400'/>  
-                </div>
-            </div>
+  return (
+    <div className="border-b border-neutral-800 pb-24 cursor-pointer">
+      <h2 className="my-20 text-center text-4xl">Technologies</h2>
+      <div className="flex flex-wrap items-center justify-center gap-4">
+        <div
+          title="Tailwind CSS"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiTailwindcss className="text-7xl text-blue-500" />
         </div>
-    );
+        <div
+          title="React JS"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <RiReactjsLine className="text-7xl text-blue-600" />
+        </div>
+        <div
+          title="NextJs"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <TbBrandNextjs className="text-7xl text-black" />
+        </div>
+        <div
+          title="Firebase"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <IoLogoFirebase className="text-7xl text-yellow-500" />
+        </div>
+        <div
+          title="MongoDB"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <SiMongodb className="text-7xl text-green-500" />
+        </div>
+        <div
+          title="NodeJs"
+          className="rounded-2xl border-4 border-neutral-800 p-4"
+        >
+          <FaNodeJs className="text-7xl text-green-600" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Technologies;
